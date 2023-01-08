91c8d1c2b920ebd12dbfa9ec7eea919b8a1d3814
Hugo Eduardo Carvajalino Solano <link_2618@hotmail.com>
2023-01-08 11:16:16 -0500

    Operadores de tiempo

---
+++ src/time-operators/01-debounceTime.ts
@@ -0,0 +1,20 @@
+import { fromEvent } from 'rxjs';
+import { debounceTime, map, distinctUntilChanged } from 'rxjs/operators';
+
+const click$ = fromEvent<MouseEvent>( document, 'click' );
+
+click$.pipe(
+    debounceTime(3000)
+);//.subscribe( console.log );
+
+// Ejemplo 2
+const input = document.createElement('input');
+document.querySelector('body').append( input );
+
+const input$ = fromEvent<KeyboardEvent>( input, 'keyup' );
+
+input$.pipe(
+    debounceTime(1000),
+    map<any, string>( ev => ev?.target?.value ),
+    distinctUntilChanged()
+).subscribe( console.log );
---
+++ src/time-operators/02-throttleTime.ts
@@ -0,0 +1,23 @@
+import { fromEvent, asyncScheduler } from 'rxjs';
+import { throttleTime, map, distinctUntilChanged } from 'rxjs/operators';
+
+const click$ = fromEvent( document, 'click' );
+
+click$.pipe(
+    throttleTime(3000)
+)//.subscribe( console.log );
+
+// Ejemplo 2
+const input = document.createElement('input');
+document.querySelector('body').append( input );
+
+const input$ = fromEvent( input, 'keyup' );
+
+input$.pipe(
+    throttleTime(400, asyncScheduler, {
+        leading: false,
+        trailing: true
+    }),
+    map<any, string>( ev => ev?.target?.value ),
+    distinctUntilChanged()
+).subscribe( console.log );
